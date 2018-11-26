afdb21e1571c13aa8d76d5b2a43a7933c7b521ff
gabrielcs23 <gabrielcs23@hotmail.com>
2018-11-25 22:03:02 -0200

    trigger para regra de funcionário maior de idade

--- Parte2_MySQL.sql
+++ Parte2_MySQL.sql
@@ -4,6 +4,8 @@ USE dev;
    2. Implementar triggers que garantam a validação das regras semânticas criadas
 ********************************************************************************/
 
+-- Regra da relação tempo/preço (se Milliseconds < 30 mili -> unitPrice = 0)
+
 DROP TRIGGER IF EXISTS min_tempo_pago_insert;
 DROP TRIGGER IF EXISTS min_tempo_pago_update;
 
@@ -34,9 +36,52 @@ UPDATE Track SET UnitPrice = 2 WHERE TrackId = 9000;
 SELECT UnitPrice FROM Track WHERE TrackId = 9000;
 DELETE FROM Track WHERE TrackId = 9000;
 
+-- Regra de que todo funcionário deve ser maior de idade
+
+DROP TRIGGER IF EXISTS func_maior_idade;
+
+DELIMITER $$
+
+CREATE TRIGGER func_maior_idade
+BEFORE INSERT ON employee
+FOR EACH ROW
+BEGIN
+	DECLARE anoNasc INT;
+    DECLARE mesNasc INT;
+    DECLARE diaNasc INT;
+    
+    DECLARE anoAtual INT;
+    DECLARE mesAtual INT;
+    DECLARE diaAtual INT;
+    
+    SET anoNasc = CAST(EXTRACT(YEAR FROM NEW.BirthDate) AS UNSIGNED);
+    SET mesNasc = CAST(EXTRACT(MONTH FROM NEW.BirthDate) AS UNSIGNED);
+    SET diaNasc = CAST(EXTRACT(DAY FROM NEW.BirthDate) AS UNSIGNED);
+    
+    SET anoAtual = CAST(EXTRACT(YEAR FROM CURDATE()) AS UNSIGNED);
+    SET mesAtual = CAST(EXTRACT(MONTH FROM CURDATE()) AS UNSIGNED);
+    SET diaAtual = CAST(EXTRACT(DAY FROM CURDATE()) AS UNSIGNED);
+    
+    IF anoAtual - anoNasc < 18 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Funcionário não pode ter menos de 18 anos';
+    ELSEIF anoAtual - anoNasc = 18 THEN
+		IF mesAtual < mesNasc THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Funcionário não pode ter menos de 18 anos';
+		ELSEIF mesAtual = mesNasc AND diaAtual < diaNasc THEN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Funcionário não pode ter menos de 18 anos';
+        END IF;
+	END IF;
+END$$
+
+DELIMITER ;
+
+INSERT INTO `Employee` (`EmployeeId`, `LastName`, `FirstName`, `Title`, `ReportsTo`, `BirthDate`, `HireDate`, `Address`, `City`, `State`, `Country`, `PostalCode`, `Phone`, `Fax`, `Email`) VALUES (9000, N'Callahan', N'Laura', N'IT Staff', 6, '2000/11/27', '2004/3/4', N'923 7 ST NW', N'Lethbridge', N'AB', N'Canada', N'T1H 1Y8', N'+1 (403) 467-3351', N'+1 (403) 467-8772', N'laura@chinookcorp.com');
+SELECT * FROM Employee WHERE EmployeeId = 9000;
+DELETE FROM Employee WHERE EmployeeId = 9000;
+
 /*******************************************************************************
    3. Implementar procedimentos armazenados (stored procedures) que garantam a validação das regras semânticas criadas
-   TODO: tentei fazer uma stored procedure pra ser chamada pelas duas triggers acima mas não funcionou
+   TODO: tentei fazer uma stored procedure pra ser chamada por min_tempo_pago_insert e min_tempo_pago_delete mas não funcionou
 ********************************************************************************/
 
 DROP PROCEDURE IF EXISTS  min_tempo_pago_prc;
